335ce411966ac2cfafaaf20082eb02bc37e6ded2
joseb <designthinker87@gmail.com>
2021-11-20 21:40:58 -0500

    Conexion a base de datos y direccionamiento hacia pantalla busqueda.

--- lib/busqueda.dart
+++ lib/busqueda.dart
@@ -1,1 +1,22 @@
-//Vacio
+import 'package:flutter/material.dart';
+
+class busqueda extends StatefulWidget{
+
+  @override
+  BusquedaApp createState() => BusquedaApp();
+
+}
+
+class BusquedaApp extends State<busqueda>{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Resultado de la busqueda")
+      ),
+    );
+
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app1/busqueda.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -12,6 +13,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner:false, home: Index());
+  }
+}
+
+class Index extends StatefulWidget{
+  @override
+  IndexStart createState() => IndexStart();
+
+}
+
+@override
+class IndexStart extends State<Index>{
+  Widget build(BuildContext context){
+
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
@@ -24,37 +39,40 @@ class MyApp extends StatelessWidget {
           //final color? foregroundColor: ,
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children:<Widget>[
-              Padding(padding: EdgeInsets.only(left: 5,top: 20,right: 5,bottom: 0),
-                child: Center(
-                  child: Container(
-                    width: 250,
-                    height: 200,
-                    child: Image.asset("image/blanco.png"),
+            child: Column(
+              children:<Widget>[
+                Padding(padding: EdgeInsets.only(left: 5,top: 20,right: 5,bottom: 0),
+                  child: Center(
+                    child: Container(
+                      width: 250,
+                      height: 200,
+                      child: Image.asset("image/blanco.png"),
+                    ),
                   ),
                 ),
-              ),
-              Padding(padding: EdgeInsets.only(left: 5,top: 20,right: 5,bottom: 0),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                    labelText: 'Busqueda',
-                  hintText: 'Palabra clave de la busqueda'
-                  )
+                Padding(padding: EdgeInsets.only(left: 5,top: 20,right: 5,bottom: 0),
+                  child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Busqueda',
+                          hintText: 'Palabra clave de la busqueda'
+                      )
+                  ),
                 ),
-              ),
-              Padding(padding: EdgeInsets.only(left: 5,top: 20,right: 5,bottom: 0),
-              child: ElevatedButton(onPressed: (){
-                print("Presiono el boton");
-              },
-                child: Text('Buscar'),
+                Padding(padding: EdgeInsets.only(left: 5,top: 20,right: 5,bottom: 0),
+                  child: ElevatedButton(onPressed: (){
+                    print("Presiono el boton");
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => busqueda()));
+                  },
+                    child: Text('Buscar'),
+                  ),
                 ),
-              ),
-            ],
-          )
+              ],
+            )
         ),
       ),
     );
   }
 }
+
